65c71cb39006f89957f9ed7440eb06fcf4cf16e5
o-mugiko <0mugiko329.rar@gmail.com>
2019-10-17 11:19:52 +0900

    plus sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 ln -sf ~/dotfiles/nvim ~/.config
+mkdir ~/.cache/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm ./installer.sh
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
 
